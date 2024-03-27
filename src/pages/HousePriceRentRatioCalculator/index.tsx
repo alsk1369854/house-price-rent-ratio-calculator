f763bf6a3049b3b1eaff1413d48b3f89011f7409
ChiaMing <alsk1369854@gmail.com>
2024-03-27 15:30:19 +0800

    feat: 完成 房價租金比計算機

--- src/pages/HousePriceRentRatioCalculator/index.tsx
+++ src/pages/HousePriceRentRatioCalculator/index.tsx
@@ -10,26 +10,48 @@ interface IHousePriceRentRatioCalculatorForm {
 }
 
 export default function HousePriceRentRatioCalculator() {
+  /**
+   * 計算結果
+   */
   const [result, setResult] = useState<string>("0");
+
+  /**
+   * 計算參數表單
+   */
   const [form] = Form.useForm<IHousePriceRentRatioCalculatorForm>();
+
+  /**
+   * 表單提交事件
+   * @param values 表單參數
+   */
   const onFormSubmit = (values: IHousePriceRentRatioCalculatorForm) => {
     const { housePricePerSquareMeter, rentalAreaSquareMeters, monthlyRent } =
       values;
+    // 計算結果
     const housePriceRentRatio =
       (housePricePerSquareMeter * rentalAreaSquareMeters) / (monthlyRent * 12);
+    // 更新結果呈現
     setResult(housePriceRentRatio.toFixed(2));
+    // 回到頁首
+    window.scrollTo(0, 0);
+  };
+
+  /**
+   * 表單重置事件
+   */
+  const onFormReset = () => {
+    // 重ㄓ表單
+    form.resetFields();
+    // 回到頁首
+    window.scrollTo(0, 0);
   };
 
   return (
-    <div className="d-flex flex-column">
+    <div className="d-flex flex-column overflow-x-hidden">
       <h2>房價租金比：{result}</h2>
       <Form<IHousePriceRentRatioCalculatorForm>
         form={form}
         name="house-price-rent-ratio-calculator-form"
-        labelAlign="left"
-        // labelCol={{ span: 64 }}
-        // wrapperCol={{ span: 64 }}
-        // layout="horizontal"
         layout="vertical"
         onFinish={onFormSubmit}
       >
@@ -41,11 +63,6 @@ export default function HousePriceRentRatioCalculator() {
               required: true,
               message: "必填",
             },
-            // ({ getFieldValue }) => ({
-            //   validator: () => {
-            //     console.log(typeof getFieldValue("housePricePerSquareMeter"));
-            //   },
-            // }),
           ]}
         >
           <InputNumber
@@ -90,7 +107,7 @@ export default function HousePriceRentRatioCalculator() {
           <Button type="primary" onClick={() => form.submit()}>
             提交
           </Button>
-          <Button htmlType="button" onClick={() => form.resetFields()}>
+          <Button htmlType="button" onClick={() => onFormReset}>
             重置
           </Button>
         </Space>
